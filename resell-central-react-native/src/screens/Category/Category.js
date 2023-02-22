68d1a61db47ca984ab72499df9cf6b6a9d708d90
waqassaidpixarsart <waqas.said@pixarsart.com>
2023-02-22 23:54:41 +0500

    fixed CategoryScreen inline styling and added floating action button

--- resell-central-react-native/src/screens/Category/Category.js
+++ resell-central-react-native/src/screens/Category/Category.js
@@ -1,294 +1,173 @@
-import { View, Text, Image, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import MainHeader from '@/Components/MainHeader/MainHeader'
-import SearchBar from '@/Components/Searchbar'
-import CheckBox from '@react-native-community/checkbox'
-import { mainBlue } from '@/Constants/Colors'
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
+import MainHeader from "@/Components/MainHeader/MainHeader";
+import SearchBar from "@/Components/Searchbar";
+import CheckBox from "@react-native-community/checkbox";
+import { mainBlue } from "@/Constants/Colors";
 
 const Category = () => {
-  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
   return (
-    <View>
+    <SafeAreaView>
       <MainHeader
         icon={
           <Image
-            style={{ height: 13, width: 20 }}
+            style={styles.headerImageStyle}
             resizeMode="center"
-            source={require('../../Assets/Images/Hamburger.png')}
+            source={require("../../Assets/Images/Hamburger.png")}
           />
         }
         heading="Categories"
       />
-      <View style={{ flexDirection: 'row', paddingRight: 28 }}>
+      <View style={styles.inputView}>
         <SearchBar />
-        <View
-          style={{
-            backgroundColor: 'white',
-            height: 50,
-            width: 50,
-            borderRadius: 5,
-            marginTop: 12,
-            // marginRight: 13,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <View style={styles.filteringIconView}>
           <Image
-            style={{ height: 60, width: 25 }}
+            style={styles.filteringIconImage}
             resizeMode="contain"
-            source={require('../../Assets/Images/system-uicons_filtering.png')}
+            source={require("../../Assets/Images/system-uicons_filtering.png")}
           />
         </View>
       </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          padding: 24,
-        }}
-      >
-        <View style={{ justifyContent: 'flex-start', flexDirection: 'row' }}>
+      <View style={styles.secondRowView}>
+        <View style={styles.checkBoxAndTextView}>
           <CheckBox
             disabled={false}
             value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
+            onValueChange={(newValue) => setToggleCheckBox(newValue)}
           />
-          <Text>Select All</Text>
+          <Text style={styles.selectAllText}>Select All</Text>
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-          <Text
-            style={{
-              paddingRight: 23,
-              fontWeight: '600',
-              fontSize: 16,
-              lineHeight: 19.49,
-              color: mainBlue,
-            }}
-          >
-            IMPORT
-          </Text>
-          <Text style={{ paddingRight: 23, fontSize: 16, lineHeight: 19.49 }}>
-            EXPORT
-          </Text>
+        <View style={styles.importExportView}>
+          <Text style={styles.importText}>IMPORT</Text>
+          <Text style={styles.exportView}>EXPORT</Text>
         </View>
       </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingLeft: 24,
-          paddingRight: 24,
-        }}
-      >
+      <View style={styles.categoriesAndProductTextView}>
         <Text>Categories</Text>
         <Text>Products</Text>
       </View>
-      <View
-        style={{
-          height: 0.2,
-          width: '88%',
-          backgroundColor: '#212121',
-          marginLeft: 24,
-          marginTop: 8,
-        }}
-      />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{ flexDirection: 'row', paddingLeft: 24 }}>
-         <View style={{paddingTop: 5}}>
-
-          <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
+      <View style={styles.line} />
+      <View style={styles.categoryItemView}>
+        <View style={styles.categoryLeftView}>
+          <View style={styles.categoryCheckBoxView}>
+            <CheckBox
+              disabled={false}
+              value={toggleCheckBox}
+              onValueChange={(newValue) => setToggleCheckBox(newValue)}
             />
-            </View>
-            <View style={{paddingTop: 10}}>
-
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/blueDots.png')}
+          </View>
+          <View style={styles.sixDotsImageView}>
+            <Image
+              style={styles.sixDotsImage}
+              resizeMode="contain"
+              source={require("../../Assets/Images/blueDots.png")}
             />
-            </View>
+          </View>
 
-          <Text style={{paddingTop: 10, paddingLeft: 3}}>Base Ball</Text>
+          <Text style={styles.categoryNameText}>Base Ball</Text>
         </View>
-        <View style={{flexDirection: 'row', paddingRight: 14, paddingTop: 13}}>
+        <View style={styles.productsColumView}>
           <Text>113</Text>
           <Image
-            style={{ height: 20, width: 25 }}
+            style={styles.productOptionImage}
             resizeMode="contain"
-            source={require('../../Assets/Images/threeDots.png')}
-            />
+            source={require("../../Assets/Images/threeDots.png")}
+          />
         </View>
       </View>
       <View
         style={{
-          height: .5,
-          width: '88%',
-          backgroundColor: '#212121',
+          height: 0.5,
+          width: "88%",
+          backgroundColor: "#212121",
           marginLeft: 24,
           marginTop: 8,
         }}
       />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{ flexDirection: 'row', paddingLeft: 24 }}>
-         <View style={{paddingTop: 5}}>
-
-          <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
-            />
-            </View>
-            <View style={{paddingTop: 10}}>
-
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/blueDots.png')}
-            />
-            </View>
-
-          <Text style={{paddingTop: 10, paddingLeft: 3}}>European Base Ball</Text>
-        </View>
-        <View style={{flexDirection: 'row', paddingRight: 14, paddingTop: 13}}>
-          <Text>113</Text>
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/threeDots.png')}
-            />
-        </View>
+      <View style={styles.floatingButtonView}>
+        <Image 
+        style={styles.floatingButton}
+        source={require('../../Assets/Images/floatingButtonImage.png')}/>
       </View>
-      <View
-        style={{
-          height: .5,
-          width: '88%',
-          backgroundColor: '#212121',
-          marginLeft: 24,
-          marginTop: 8,
-        }}
-      />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{ flexDirection: 'row', paddingLeft: 24 }}>
-         <View style={{paddingTop: 5}}>
+    </SafeAreaView>
+  );
+};
 
-          <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
-            />
-            </View>
-            <View style={{paddingTop: 10}}>
+const styles = StyleSheet.create({
+  headerImageStyle: {
+    height: 13,
+    width: 20,
+  },
+  inputView: {
+    flexDirection: "row",
+    paddingRight: 28,
+  },
+  filteringIconView: {
+    backgroundColor: "white",
+    height: 50,
+    width: 50,
+    borderRadius: 5,
+    marginTop: 12,
+    // marginRight: 13,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filteringIconImage: {
+    height: 60,
+    width: 25,
+  },
+  secondRowView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 24,
+  },
+  checkBoxAndTextView: { justifyContent: "flex-start", flexDirection: "row" },
+  selectAllText: { padding: 6 },
+  importExportView: { flexDirection: "row", justifyContent: "space-evenly" },
+  importText: {
+    paddingRight: 23,
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 19.49,
+    color: mainBlue,
+  },
+  exportView: { paddingRight: 23, fontSize: 16, lineHeight: 19.49 },
+  categoriesAndProductTextView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  line: {
+    height: 0.2,
+    width: "88%",
+    backgroundColor: "#212121",
+    marginLeft: 24,
+    marginTop: 8,
+  },
+  categoryItemView: { flexDirection: "row", justifyContent: "space-between" },
+  categoryLeftView: { flexDirection: "row", paddingLeft: 24 },
+  categoryCheckBoxView: { paddingTop: 5 },
+  sixDotsImageView: { paddingTop: 10 },
+  sixDotsImage: { height: 20, width: 25 },
+  categoryNameText: { paddingTop: 10, paddingLeft: 3 },
+  productsColumView: { flexDirection: "row", paddingRight: 14, paddingTop: 13 },
+  productOptionImage: { height: 20, width: 25 },
+  floatingButtonView: {
+    paddingTop: 485,
+    paddingLeft: 345
+  },
+  floatingButton: {
+    height: 45,
+    width: 45
+  }
+});
 
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/blueDots.png')}
-            />
-            </View>
-
-          <Text style={{paddingTop: 10, paddingLeft: 3}}>Australian Base Ball</Text>
-        </View>
-        <View style={{flexDirection: 'row', paddingRight: 14, paddingTop: 13}}>
-          <Text>113</Text>
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/threeDots.png')}
-            />
-        </View>
-      </View>
-      <View
-        style={{
-          height: .5,
-          width: '88%',
-          backgroundColor: '#212121',
-          marginLeft: 24,
-          marginTop: 8,
-        }}
-      />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{ flexDirection: 'row', paddingLeft: 24 }}>
-         <View style={{paddingTop: 5}}>
-
-          <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
-            />
-            </View>
-            <View style={{paddingTop: 10}}>
-
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/blueDots.png')}
-            />
-            </View>
-
-          <Text style={{paddingTop: 10, paddingLeft: 3}}>Football Ball</Text>
-        </View>
-        <View style={{flexDirection: 'row', paddingRight: 14, paddingTop: 13}}>
-          <Text>113</Text>
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/threeDots.png')}
-            />
-        </View>
-      </View>
-      <View
-        style={{
-          height: .5,
-          width: '88%',
-          backgroundColor: '#212121',
-          marginLeft: 24,
-          marginTop: 8,
-        }}
-      />
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <View style={{ flexDirection: 'row', paddingLeft: 24 }}>
-         <View style={{paddingTop: 5}}>
-
-          <CheckBox
-            disabled={false}
-            value={toggleCheckBox}
-            onValueChange={newValue => setToggleCheckBox(newValue)}
-            />
-            </View>
-            <View style={{paddingTop: 10}}>
-
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/blueDots.png')}
-            />
-            </View>
-
-          <Text style={{paddingTop: 10, paddingLeft: 3}}>Beauty Products</Text>
-        </View>
-        <View style={{flexDirection: 'row', paddingRight: 14, paddingTop: 13}}>
-          <Text>113</Text>
-          <Image
-            style={{ height: 20, width: 25 }}
-            resizeMode="contain"
-            source={require('../../Assets/Images/threeDots.png')}
-            />
-        </View>
-      </View>
-      <View
-        style={{
-          height: .5,
-          width: '88%',
-          backgroundColor: '#212121',
-          marginLeft: 24,
-          marginTop: 8,
-        }}
-      />
-    </View>
-  )
-}
-
-export default Category
+export default Category;
