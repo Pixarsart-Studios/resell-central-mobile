1b20dcf229d5f5f440ba412ac72412080da05c70
waqassaidpixarsart <waqas.said@pixarsart.com>
2023-03-01 16:22:39 +0500

    accounting screens completed

--- resell-central-react-native/src/Translations/resources/en.js
+++ resell-central-react-native/src/Translations/resources/en.js
@@ -10,8 +10,43 @@ export default {
     signIn: 'SIGN IN',
     dontHaveAccount: "Don't have account.",
     signUp: 'Sign UP',
-    orSignInWith : 'Or Sign in with'
+    orSignInWith : 'Or Sign in with',
+    
 
+
+
+   ///////  Accounting Screens
+    accounting: 'Accounting',
+    activePlan: 'Active Plan',
+    businessPlan: 'Business Plan',
+    $300: '$300.00',
+    mon: '/mon',
+    reNewOn: 'Renew on:',
+    reNewDate: '20-02-2023',
+    add : 'ADD',
+    expense: 'EXPENSE',
+    stats: 'Stats',
+    day: 'Day',
+    week: 'Week',
+    month: 'Month',
+    year: 'Year',
+    mon: 'Mon',
+    tue: 'Tue',
+    wed: 'Wed',
+    thu: 'Thu',
+    fri: 'Fri',
+    sat: 'Sat',
+    sun: 'Sun',
+    profit: 'Profit',
+    expenses: 'Expenses',
+    history: 'History', 
+    addExpense: 'Add Expense',
+    marketing: 'Marketing',
+    inventory: 'Inventory',
+    promotions: 'Promotions',
+    investments: 'Investments',
+    ADDMORE: 'ADD MORE',
+    ADDEXPENSES : "ADD EXPENSES"
     },
   fr: {
     welcome: 'nous sommes localisés maintenant',
