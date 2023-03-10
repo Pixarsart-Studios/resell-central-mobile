7f7f988460aa1bd64a94980ce2389a723dc13682
asimbashir <asim@pixarsart.com>
2023-03-10 23:02:53 +0500

    bottom tab icons

--- resell-central-react-native/src/Navigators/Main.js
+++ resell-central-react-native/src/Navigators/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image, Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "@/screens/Home/Home";
 import Notifications from "@/screens/Nofitications/Notifications";
@@ -9,7 +10,22 @@ import Dashboard from "@/screens/Dashboard/Dashboard";
 const Tab = createBottomTabNavigator();
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          // backgroundColor: theme.white_off,
+          // flex: 1,
+          elevation: 0,
+          height: Platform.OS === "ios" ? 50 : 50,
+          // width: "100%",
+          // borderRadius: 7,
+          // alignSelf: "center",
+          // bottom: 10,
+          // alignItems: 'center',
+          // justifyContent: 'center'
+        },
+      }}
+    >
       {/* <Tab.Screen name="DashboardScreen" component={Dashboard} /> */}
 
       <Tab.Screen
@@ -17,7 +33,22 @@ const MainNavigator = () => {
         component={Dashboard}
         options={{
           headerShown: false,
-          tabBarIconStyle: { display: "none" },
+          tabBarIcon: (focused) => (
+            <Image
+              source={require("../Assets/Images/home.png")}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: focused ? "#4D559F" : "black",
+              }}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#4D559F",
+            fontWeight: "700",
+            paddingBottom: 5,
+          },
+          // tabBarIconStyle: { display: "none" },
           // tabBarLabelPosition: 'beside-icon',
         }}
       />
@@ -26,25 +57,65 @@ const MainNavigator = () => {
         component={Notifications}
         options={{
           headerShown: false,
-          tabBarIconStyle: { display: "none" },
+          tabBarIcon: (focused) => (
+            <Image
+              source={require("../Assets/Images/notification.png")}
+              style={{
+                width: 17,
+                height: 20,
+                tintColor: focused ? "#4D559F" : "black",
+              }}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#4D559F",
+            fontWeight: "700",
+            paddingBottom: 5,
+          },
         }}
       />
       <Tab.Screen
         name="Orders"
         component={Orders}
         options={{
-          tabBarIconStyle: { display: "none" },
           headerShown: false,
-          // tabBarLabelPosition: 'beside-icon',
+          tabBarIcon: (focused) => (
+            <Image
+              source={require("../Assets/Images/manageOrdersIcon.png")}
+              style={{
+                width: 17,
+                height: 20,
+                tintColor: focused ? "#4D559F" : "black",
+              }}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#4D559F",
+            fontWeight: "700",
+            paddingBottom: 5,
+          },
         }}
       />
       <Tab.Screen
         name="Account"
         component={Account}
         options={{
-          tabBarIconStyle: { display: "none" },
           headerShown: false,
-          //  tabBarLabelPosition: 'beside-icon',
+          tabBarIcon: (focused) => (
+            <Image
+              source={require("../Assets/Images/account.png")}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: focused ? "#4D559F" : "black",
+              }}
+            />
+          ),
+          tabBarLabelStyle: {
+            color: "#4D559F",
+            fontWeight: "700",
+            paddingBottom: 5,
+          },
         }}
       />
     </Tab.Navigator>
